5890ce6058e5f67461dbb8b538e077c10b0cca9e
TigGev <tigogevorgyan29@gmail.com>
2025-03-23 12:30:58 +0400

    adding test file for hero

--- tests/hero_test.cpp
+++ tests/hero_test.cpp
@@ -12,7 +12,7 @@
 
 
 
-// Item
+
 Item::Item(Type type, const std::string& name, const std::string& description, int value)
     : m_type(type), m_name(name), m_description(description), m_value(value) {}
 Item::~Item() {}
@@ -21,7 +21,7 @@ std::string Item::getDescription() const { return m_description; }
 const Type& Item::getType() const { return m_type; }
 int Item::getValue() const { return m_value; }
 
-// Inventory
+
 Inventory::Inventory() {}
 Inventory::~Inventory() {
     for (Item* item : items) delete item;
@@ -39,7 +39,7 @@ Item* Inventory::getItem(int index) const {
 std::vector<Item*> Inventory::getItems() const { return items; }
 int Inventory::getSize() const { return items.size(); }
 
-// NPC
+
 NPC::NPC(const std::string& name, const std::string& dialogue, const std::string& quest)
     : Character(name, 50, 0, 0), dialogue(dialogue), associatedQuest(new Quest(quest)) {}
 NPC::~NPC() { delete associatedQuest; }
@@ -49,7 +49,7 @@ void NPC::interact(Character* target) { std::cout << dialogue << "\n"; }
 std::string NPC::getDialogue() const { return dialogue; }
 Quest* NPC::getAssociatedQuest() const { return associatedQuest; }
 
-// Monster
+
 Monster::Monster(const std::string& name, int health, int attackPower, int defense, const std::string& ability)
     : Character(name, health, attackPower, defense), specialAbility(ability) {}
 Monster::~Monster() {}
@@ -58,14 +58,7 @@ void Monster::takeDamage(int damage) { m_health -= damage; if (m_health < 0) m_h
 void Monster::attack(Character* target) { target->takeDamage(m_attackPower); }
 void Monster::useAbility(Character* target) {}
 
-// // Limits (заглушка)
-// namespace Limits {
-//     const int HERO_MAX_HEALT = 100;
-//     const int HERO_MAX_ATTAC_POWER = 50;
-//     const int HERO_MAX_XP = 1000;
-// }
 
-// Тестовый фикстур для Hero
 class HeroTest : public ::testing::Test {
 protected:
     void SetUp() override {
@@ -82,7 +75,7 @@ protected:
     QuestManager* qm;
 };
 
-// Тест конструктора и displayStats
+
 TEST_F(HeroTest, ConstructorAndDisplayStats) {
     std::ostringstream oss;
     std::streambuf* oldCout = std::cout.rdbuf(oss.rdbuf());
@@ -98,7 +91,7 @@ TEST_F(HeroTest, ConstructorAndDisplayStats) {
     EXPECT_TRUE(output.find("XP: 0") != std::string::npos);
 }
 
-// Тест takeDamage
+
 TEST_F(HeroTest, TakeDamage) {
     std::ostringstream oss;
     std::streambuf* oldCout = std::cout.rdbuf(oss.rdbuf());
@@ -106,11 +99,11 @@ TEST_F(HeroTest, TakeDamage) {
     std::cout.rdbuf(oldCout);
 
     std::string output = oss.str();
-    EXPECT_EQ(hero->getHealt(), 75); // 80 - (10 - 5 defense) = 75
+    EXPECT_EQ(hero->getHealt(), 75); 
     EXPECT_TRUE(output.find("Elara takes 5 damage") != std::string::npos);
 }
 
-// Тест takeDamage с летальным исходом
+
 TEST_F(HeroTest, TakeDamageFatal) {
     std::ostringstream oss;
     std::streambuf* oldCout = std::cout.rdbuf(oss.rdbuf());
@@ -122,12 +115,12 @@ TEST_F(HeroTest, TakeDamageFatal) {
     EXPECT_TRUE(output.find("Game over! Elara is died.") != std::string::npos);
 }
 
-// Тест interact с NPC (имитация ввода)
+
 TEST_F(HeroTest, InteractWithNPC_AcceptQuest) {
     NPC npc("Villager", "Help me!", "Slay the Troll");
     std::ostringstream oss;
     std::streambuf* oldCout = std::cout.rdbuf(oss.rdbuf());
-    std::istringstream iss("1\n"); // Имитация ввода "1"
+    std::istringstream iss("1\n"); 
     std::streambuf* oldCin = std::cin.rdbuf(iss.rdbuf());
 
     hero->interact(&npc);
@@ -141,7 +134,7 @@ TEST_F(HeroTest, InteractWithNPC_AcceptQuest) {
     EXPECT_TRUE(output.find("Slay the Troll") != std::string::npos);
 }
 
-// Тест attack на монстра
+
 TEST_F(HeroTest, AttackMonster) {
     Monster monster("Goblin", 30, 10, 2, "Poison Sting");
     std::ostringstream oss;
@@ -150,11 +143,11 @@ TEST_F(HeroTest, AttackMonster) {
     std::cout.rdbuf(oldCout);
 
     std::string output = oss.str();
-    EXPECT_EQ(monster.getHealt(), 17); // 30 - (15 - 2 defense) = 17
+    EXPECT_EQ(monster.getHealt(), 17); 
     EXPECT_TRUE(output.find("Elara attacks Goblin for 15 damage!") != std::string::npos);
 }
 
-// Тест addItem и useItem (Potion)
+
 TEST_F(HeroTest, AddAndUsePotion) {
     Item* potion = new Item(Type::Potion, "Health Potion", "Restores 20 HP", 20);
     hero->addItem(potion);
@@ -166,12 +159,12 @@ TEST_F(HeroTest, AddAndUsePotion) {
     std::cout.rdbuf(oldCout);
 
     std::string output = oss.str();
-    EXPECT_EQ(hero->getHealt(), 100); // 80 + 20, но не больше 100
-    EXPECT_EQ(hero->getInventorySize(), 0); // Зелье удалено
+    EXPECT_EQ(hero->getHealt(), 100); 
+    EXPECT_EQ(hero->getInventorySize(), 0); 
     EXPECT_TRUE(output.find("Health Potion was applied") != std::string::npos);
 }
 
-// Тест useItem (Weapon)
+
 TEST_F(HeroTest, UseWeapon) {
     Item* weapon = new Item(Type::Weapon, "Sword", "Increases attack by 10", 10);
     hero->addItem(weapon);
@@ -183,21 +176,21 @@ TEST_F(HeroTest, UseWeapon) {
     std::cout.rdbuf(oldCout);
 
     std::string output = oss.str();
-    EXPECT_EQ(hero->getAttacPower(), 25); // 15 + 10 = 25
-    EXPECT_EQ(hero->getInventorySize(), 1); // Оружие не удаляется
+    EXPECT_EQ(hero->getAttacPower(), 25); 
+    EXPECT_EQ(hero->getInventorySize(), 1); 
     EXPECT_TRUE(output.find("Elara equips Sword") != std::string::npos);
 }
 
-// Тест gainXP и levelUp
+
 TEST_F(HeroTest, GainXPAndLevelUp) {
     hero->gainXP(50);
     EXPECT_EQ(hero->getEXP(), 50);
-    hero->gainXP(50); // Всего 100 XP
+    hero->gainXP(50); 
     EXPECT_EQ(hero->getEXP(), 100);
-    EXPECT_EQ(hero->getLevel(), 2); // Уровень должен увеличиться
+    EXPECT_EQ(hero->getLevel(), 2); 
 }
 
-// Тест setMount и getMount
+
 TEST_F(HeroTest, SetAndGetMount) {
     Mount* horse = new Mount("Horse");
     hero->setMount(horse);
