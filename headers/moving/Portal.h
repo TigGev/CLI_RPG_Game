97a8badc6799cb6adfb79bf383027c116b9e791b
TigGev <tigogevorgyan29@gmail.com>
2025-03-25 22:00:41 +0400

    Add Portal

--- headers/moving/Portal.h
+++ headers/moving/Portal.h
@@ -1,25 +1,27 @@
 #ifndef PORTAL_H
 #define PORTAL_H
 #include <string>
+#include "../gameWorld/Location.h"
 
 class Inventory;
 
 class Portal {
     private:
-        std::string name;
-        std::string sourceLocation;
-        std::string destinationLocation;
-        bool isLocked;
-        std::string activationCondition;
-        std::string riddleAnswer;
+        std::string m_name;
+        // std::string sourceLocation;
+        Location m_sourceLocation;
+        Location m_destinationLocation;
+        bool m_isLocked;
+        std::string m_activationCondition;
+        std::string m_Answer;
     public:
-        Portal(const std::string& source, const std::string& destination, bool locked, 
+        Portal(const std::string& name, Location source, Location destination, bool locked, 
                const std::string& condition, const std::string& answer = "") ;
-        ~Portal();
+        ~Portal() = default;
         bool attemptActivation(const std::string& playerInput, Inventory& playerInventory);
-        std::string getSource() const;
+        Location getSource() const;
         std::string getName() const;
-        std::string getDestination() const;
+        Location getDestination() const;
         bool isPortalLocked() const;
         std::string getCondition() const;
 };
---
+++ src/moving/Portal.cpp
@@ -0,0 +1,25 @@
+#include "../../headers/moving/Portal.h"
+#include "../../headers/utility/Utility.h"
+
+Portal::Portal(const std::string& name, Location source, Location destination, bool locked, 
+    const std::string& condition, const std::string& answer = "") 
+        : m_name(name), m_sourceLocation(source), m_destinationLocation(destination), m_isLocked(locked),
+            m_activationCondition(condition), m_Answer(answer) {}
+
+bool Portal::attemptActivation(const std::string& playerInput, Inventory& playerInventory) {
+    if (playerInput == m_activationCondition) {
+        m_isLocked = true;
+        return true;
+    }
+    return false;
+}
+
+Location Portal::getSource() const {return m_sourceLocation;}
+
+Location Portal::getDestination() const {return m_destinationLocation;}
+
+std::string Portal::getName() const {return m_name;}
+
+bool Portal::isPortalLocked() const {return m_isLocked;}
+
+std::string Portal::getCondition() const {return m_activationCondition;}
